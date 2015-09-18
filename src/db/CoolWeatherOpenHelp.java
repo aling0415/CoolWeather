6e68d2a7b2cc0417586fbc267f26747a67222eb7
haha <haha@gmail.com>
2015-09-18 13:01:07 +0800

    新增数据å库文件到，以及个各表对应的实体çâ

---
+++ src/db/CoolWeatherOpenHelp.java
@@ -0,0 +1,43 @@
+package db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelp extends SQLiteOpenHelper {
+	
+    //province建表语句
+	public static final String CREAT_PROVINCE = "create table Province("+
+    "id integer primary key autoincrement,"+"province_name text,"+"province_code text)";
+	
+	//city建表语句
+	public static final String CREAT_CITY = "create table City("+
+			"id integer primary key autoincrement,"+"city_name text,"+"province_id integer,"+"city_code text)";
+	
+	//country建表语句
+	public static final String CREAT_COUNTRY = "create table Country("+
+			"id integer primary key autoincrement,"+"country_name text,"+"city_id integer,"+"country_code text)";
+	
+
+	public CoolWeatherOpenHelp(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(CREAT_PROVINCE);   //创建省表
+		db.execSQL(CREAT_CITY);      //创建城市表  
+		db.execSQL(CREAT_COUNTRY);    //创建country表
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
